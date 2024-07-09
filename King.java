faf863bcd49872ae4cd1651c1aef10d6fa5d5606
Monica Graham <monicagraham40@gmail.com>
2024-07-09 16:21:08 -0400

    king queen update

---
+++ King.java
@@ -0,0 +1,15 @@
+public class King extends Piece 
+{
+    private int value = 1;
+    
+    public King(String color)
+    {
+        super(color);
+    }
+
+    
+    public int getValue()
+    {
+        return this.value;
+    }
+}
---
+++ Queen.java
@@ -0,0 +1,15 @@
+public class Queen extends Piece 
+{
+    private int value = 12;
+    
+    public Queen(String color)
+    {
+        super(color);
+    }
+
+    
+    public int getValue()
+    {
+        return this.value;
+    }
+}
